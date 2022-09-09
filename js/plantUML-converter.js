5982e85c7c2ff8635fb7ef8e91ead1c3ff7af89f
Sara Maia <sara.maia@zensurance.com>
2022-09-09 10:17:42 -0700

    improves UML formatting

--- js/plantUML-converter.js
+++ js/plantUML-converter.js
@@ -5,12 +5,33 @@ const folderPath = "/Users/Sara.Maia/Dev/zen/";
 function createPlantUmlText(tourStops) {
   const order = getIndexOrder(tourStops);
   const uml = [];
-  uml.push("@startuml", "!theme aws-orange");
+  const routeMap = new Map();
+  uml.push(
+    "@startuml",
+    "!theme aws-orange",
+    "actor Client",
+    `Client -> ${tourStops[order[0].index].fileName.replace(
+      /-/g,
+      ""
+    )}: placeholder message`
+  );
   for (let i = 0; i < order.length - 1; i++) {
     const origin = tourStops[order[i].index].fileName;
     const dest = tourStops[order[i + 1].index].fileName;
+    if (!findBetween(origin, 0, i, order, tourStops)) {
+      uml.push("activate " + origin.replace(/-/g, ""));
+    }
     if (origin != dest) {
-      uml.push(formatUmlLine(origin, dest, "placeholder message"));
+      const isReturn = routeMap.get(dest) === origin;
+      uml.push(formatUmlLine(origin, dest, "placeholder message", isReturn));
+      if (isReturn) {
+        routeMap.delete(dest);
+      } else {
+        routeMap.set(origin, dest);
+      }
+    }
+    if (!findBetween(origin, i + 1, order.length, order, tourStops)) {
+      uml.push("deactivate " + origin.replace(/-/g, ""));
     }
   }
   uml.push("@enduml");
@@ -41,8 +62,21 @@ function getIndexOrder(objArr) {
   return order.sort((a, b) => a.step - b.step);
 }
 
-function formatUmlLine(start, end, message) {
-  return `${start.replace(/-/g, "")} -> ${end.replace(/-/g, "")} : ${message}`;
+function findBetween(name, startIndex, endIndex, order, tourObjs) {
+  let includesName = false;
+  for (let i = startIndex; i < endIndex; i++) {
+    if (tourObjs[order[i].index].fileName === name) {
+      includesName = true;
+    }
+  }
+  return includesName;
+}
+
+function formatUmlLine(start, end, message, isReturn) {
+  return `${start.replace(/-/g, "")} ${isReturn ? "-->" : "->"} ${end.replace(
+    /-/g,
+    ""
+  )} : ${message}`;
 }
 
 module.exports = {
